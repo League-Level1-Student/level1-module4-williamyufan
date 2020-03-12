6f72cf9fcca36b07d5d797b29fb0032276cadb40
league <league@imac-22.attlocal.net>
2020-03-11 19:58:18 -0700

    start step 16 next time

--- src/_01_nasty_surprise/nasty_suprise.java
+++ src/_01_nasty_surprise/nasty_suprise.java
@@ -1,25 +1,39 @@
 package _01_nasty_surprise;
 
 import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class nasty_suprise {
+public class nasty_suprise implements ActionListener {
+	
+	JButton u=new JButton();
+	JButton h=new JButton();
 public static void main(String[] args) {
-Button u=new Button();
-Button h=new Button();
-u.addActionListener(null);
-h.addActionListener(l);
-u.setName("Trick");
-h.setName("Treat");
-
+nasty_suprise o=new nasty_suprise();
+o.setup();
 }
 
+public void setup(){
+	JFrame i=new JFrame();
+	JPanel p=new JPanel();
+	i.add(p);
+	i.setVisible(true);
+	p.add(u);
+	p.add(h);
+	u.addActionListener(this);
+	h.addActionListener(this);
+	u.setText("Trick");
+	h.setText("Treat");
+}
 
 private void showPictureFromTheInternet(String imageUrl) {
     try {
@@ -33,4 +47,16 @@ private void showPictureFromTheInternet(String imageUrl) {
     } catch (MalformedURLException e) {
         e.printStackTrace();
     }
-}}
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton buttonPressed = (JButton) e.getSource();
+	if(buttonPressed==h) {
+		showPictureFromTheInternet("https://i.ytimg.com/vi/a6KGPBflhiM/hqdefault.jpg");}
+	else {
+			showPictureFromTheInternet("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rev-1-it2-26273r-high-res-jpeg-1567721576.jpg?crop=0.531xw:0.796xh;0.471xw,0.0383xh&resize=480:*");
+		}
+	}
+}
--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -30,16 +30,23 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
+JFrame u;
 
 	public void run() {
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
 		// 2. Add the four images that match keyboard keys like this:
 		// images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
 
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
 		// key when
 		// 'Simon says' otherwise press a different key"
 
 		// 4. Call the showImage method to show an image
+		showImage();
 
 	}
 
@@ -73,27 +80,32 @@ public class SimonSays extends KeyAdapter {
 	}
 
 	private void showImage() {
+		u=new JFrame();
+		u.setVisible(true);
+		
 		// 5. Initialize your frame to a new JFrame()
 
 		// 6. Set the frame to visible
 
 		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
+		 u.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+		 u.setName("Yes");
 		// 9. Pack the frame
-
+		 u.pack();
 		// 10. Set the defaultCloseOperation of your frame to
-		// JFrame.EXIT_ON_CLOSE
+		 u.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 
 		// 11. Add a key listener to the frame
-
+		 u.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+		 Random i=new Random();
+		 int o=i.nextInt(100);
 		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
-
+		 
 		// 14. Above, set the value of simonSays to true/false appropriately
 
 	}
